16dc0f43894564c1055f74079c9d614cd56cca9a
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-03-11 00:04:46 +0600

    fixed big status issue and 30 character

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -110,8 +110,8 @@ const createPost = (post) => {
 
                   <div class="post__description">
                     <small>
-                      <a class="post__name--underline" href="#">${post.comments.user}
-                      </a>${post.comments?.text}
+                      <a class="post__name--underline" href="#">${post.comments[0].user}
+                      </a>${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
