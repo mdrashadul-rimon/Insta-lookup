034c932b5733d124e51a6012204088e171b0db90
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-03-11 02:48:28 +0600

    fixed all bugs

--- js/app.js
+++ js/app.js
@@ -133,15 +133,20 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+  document.getElementById("liked").innerHTML = "";
   likedPosts.forEach((post) => {
     const div = createPost(post);
+
     document.getElementById("liked").appendChild(div);
+
   });
 };
 
 const displayReportedPosts = () => {
+  document.getElementById("reported").innerHTML = "";
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
